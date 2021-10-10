70b46d0693a735c8397675464b934433ed0b2ab4
weburnit <weburnittv@gmail.com>
2021-10-10 16:21:27 +0700

    [master] releae rinkeby

--- migrations/4_deploy_exchange_contracts.js
+++ migrations/4_deploy_exchange_contracts.js
@@ -9,24 +9,39 @@ module.exports = async function (deployer, network) {
     const token = await Token.deployed()
 
     const Sale = {
-        test: BNBSale,
-        development: BNBSale,
-        testbsc: BNBSale,
-        bsc: BNBSale,
+        test: 'bnb',
+        development: 'bnb',
+        testbsc: 'bnb',
+        bsc: 'bnb',
 
-        mainnet: EthSale,
-        rinkeby: EthSale,
-        ropsten: EthSale
+        mainnet: 'eth',
+        rinkeby: 'eth',
+        ropsten: 'eth'
     }
     // Deploy TimeLock
+    const sale = Sale[network];
     const timeLock = await deployer.deploy(TokenTimeLock);
-
     const factory = await deployProxy(TokenTimeLockFactory, [timeLock.address], {kind: 'uups', deployer: deployer});
-    console.log("Deployed to address ", factory.address);
+    console.log("Deployed to Lock factory address ", factory.address);
 
     // Deploy BNB Exchange
-    await deployProxy(Sale[network], [token.address, factory.address], {kind: 'uups', deployer: deployer});
-    const exchange = await Sale[network].deployed();
-    await token.approve(exchange.address, web3.utils.toWei('9000000000'));
-    console.log("Allowance for exchange", (await token.allowance(await token.owner(), exchange.address)).toString())
+    if (sale === 'bnb') {
+        await deployBNB(deployer, token, factory.address);
+    } else {
+        await deployETH(deployer, token, factory.address);
+    }
 };
+
+async function deployBNB(deployer, token, factoryAddress) {
+    // Deploy BNB Exchange
+    await deployProxy(BNBSale, [token.address, factoryAddress], {kind: 'uups', deployer: deployer});
+    const exchange = await BNBSale.deployed();
+    await token.approve(exchange.address, web3.utils.toWei('9000000000'));
+}
+
+async function deployETH(deployer, token, factoryAddress) {
+    // Deploy ETH Exchange
+    await deployProxy(EthSale, [token.address, factoryAddress], {kind: 'uups', deployer: deployer});
+    const exchange = await EthSale.deployed();
+    await token.approve(exchange.address, web3.utils.toWei('9000000000'));
+}
